015473a9eabf150ae27369befb27c44940095e8f
Halyson Reis <halysondasilvadosreis@gmail.com>
2024-10-30 16:54:08 -0400

    task01

---
+++ exemplovariavel.php
@@ -0,0 +1,11 @@
+<?php
+
+    $nome = "Halyson";
+    $sobrenome = "Reis";
+    $idade = "18";
+    $profissao = "Aluno";
+ 
+    echo "oi $profissao $nome $sobrenome. <br>";
+    echo "sua idade $idade";
+
+?>
